b7425658f9eadf4c65dd310740f84f6f93670b8b
Esdras Silva <estudos14j@gmail.com>
2022-08-12 17:15:04 -0300

    modified:   PR_completo00.py

--- PR_completo00.py
+++ PR_completo00.py
@@ -5,68 +5,71 @@ def partida():
     
     if (n % (m + 1) == 0):
         print("Você começa!!!")
-        cond_player = False
+        cond_player = True
         tirar = usuario_escolhe_jogada(n, m)
         print("Você tirou", tirar)
         n = n - tirar
         if n <= 0:
-            cond = False
             print("Você ganhou!")
-            return("Você ganhou!")
+            point_player = "jogador"
+            return(point_player)
     else:
+        cond_player = False
         print("Computador começa!!!")
-        cond_player = True
         tirar = computador_escolhe_jogada(n, m)
         print("Computador tirou", tirar)
         n = n - tirar
         if n <= 0:
-            cond = False
             print("computador ganhou!")
-            return("computador ganhou!")
+            point_computer = "computador"
+            return(point_computer)
         else:
             print("Sobrou", n)
-            if cond_player:
-                while n != 0:
-                        tirar = usuario_escolhe_jogada(n, m)
-                        print("Você tirou", tirar)
-                        n = n - tirar
-                        if n <= 0:
-                            cond = False
-                            print("Você ganhou!")
-                            return("Você ganhou!")
-                        else:
-                            print("Sobrou", n)
-                            tirar = computador_escolhe_jogada(n, m)
-                            print("Computador tirou", tirar)
-                            n = n - tirar
-                            if n <= 0:
-                                cond = False
-                                print("computador ganhou!")
-                                return("computador ganhou!")
-                            else:
-                                print("Sobrou", n,"peças.")
-            else:
-                while n != 0:
+    if not cond_player:
+        while cond:
+                tirar = usuario_escolhe_jogada(n, m)
+                print("Você tirou", tirar)
+                n = n - tirar
+                if n <= 0:
+                    print("Você ganhou!")
+                    point_player = "jogador"
+                    return(point_player)
+                    cond = False
+                else:
+                    print("Sobrou", n)
                     tirar = computador_escolhe_jogada(n, m)
                     print("Computador tirou", tirar)
                     n = n - tirar
                     if n <= 0:
-                        cond = False
                         print("computador ganhou!")
-                        return("computador ganhou!")
+                        point_compuint = "computador"
+                        return(point_compuint)
+                        cond = False
                     else:
                         print("Sobrou", n,"peças.")
-                        tirar = usuario_escolhe_jogada(n, m)
-                        print("Você tirou", tirar)
-                        n = n - tirar
-                        if n <= 0:
-                            cond = False
-                            print("Você ganhou!")
-                            return("Você ganhou!")
-                        else:
-                            print("Sobrou", n)
+    else:
+        while cond:
+            tirar = computador_escolhe_jogada(n, m)
+            print("Computador tirou", tirar)
+            n = n - tirar
+            if n <= 0:
+                print("computador ganhou!")
+                point_compuint = "computador"
+                return(point_compuint)
+                cond = False
+            else:
+                print("Sobrou", n,"peças.")
+                tirar = usuario_escolhe_jogada(n, m)
+                print("Você tirou", tirar)
+                n = n - tirar
+                if n <= 0:
+                    print("Você ganhou!")
+                    point_player = "jogador"
+                    return(point_player)
+                    cond = False
+                else:
+                    print("Sobrou", n)
 
-                
 def usuario_escolhe_jogada(n, m):
     while n != 0:
         tirar = int(input("Quantas peças você vai tirar? "))
@@ -74,7 +77,6 @@ def usuario_escolhe_jogada(n, m):
             print("Oops! Jogada inválida! Tente de novo.")
         else:
             return(tirar)
-
 
 def computador_escolhe_jogada(n, m):
     tirar = n % (m + 1)
@@ -92,48 +94,44 @@ def inicio():
     print("Bem-vindo ao jogo do NIM! Escolha: ")
     print("")
     cond = True
-    placar_jogador = 0
     placar_computer = 0
-
-
-    print("1- para jogar uma partida isolada")
-    print("2- para jogar um campeonato")
-    mode = input("")
-    print("")
-
-    if mode == "1":
-        print("Voce escolheu uma partida isolada! ")
-        partida()
-        cond = False
-    else:
-        if mode == "2":
-            print("Voce escolheu um campeonato!")
-            
-            print("**** Rodada 1 ****")
-            
+    placar_player = 0
+    
+    while cond:
+        print("1- para jogar uma partida isolada")
+        print("2- para jogar um campeonato")
+        mode = input()
+        print("")
+        
+        if mode == 1:
+            print("Voce escolheu uma partida isolada! ")
             partida()
-            if partida() == "Você ganhou":
-                placar_jogador = placar_jogador + 1
-            else:
-                placar_computer = placar_computer + 1
-            
-            print("**** Rodada 2 ****")
-            
-            partida()
-            if partida() == "Você ganhou":
-                placar_jogador = placar_jogador + 1
-            else:
-                placar_computer = placar_computer + 1
+            cond = False
+        else:
+            if mode == "2":
+                print("Voce escolheu um campeonato!")
                 
-            print("**** Rodada 3 ****")
-            
-            partida()
-            if partida() == "Você ganhou":
-                placar_jogador = placar_jogador + 1
+                print("**** Rodada 1 ****")
+                if partida() == "computador":
+                    placar_computer = placar_computer + 1
+                else:
+                    placar_player = placar_player + 1
+                
+                print("**** Rodada 2 ****")
+                if partida() == "computador":
+                    placar_computer = placar_computer + 1
+                else:
+                    placar_player = placar_player + 1
+                    
+                print("**** Rodada 3 ****")
+                if partida() == "computador":
+                    placar_computer = placar_computer + 1
+                else:
+                    placar_player = placar_player + 1
+                    
+                print("****Final do campeonato! ****")
+                print("Placar: Você", placar_player,"X", placar_computer, "Computador")
+                cond = False
             else:
-                placar_computer = placar_computer + 1
-
-            print("****Final do campeonato! ****")
-            
-            print("Placar: Você", placar_jogador,"X", placar_computer, "Computador")
+                print("Oops! Escolha inválida! Tente de novo.")
 inicio()
